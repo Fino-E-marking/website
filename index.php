002ef7187b1a208c04bc450db59300dec72fc0b4
fino DEV <felixnyiene@gmail.com>
2024-02-20 00:57:29 +0100

    after placing retriction on password

--- index.php
+++ index.php
@@ -15,7 +15,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
   $hash = password_hash($password, PASSWORD_DEFAULT);
   if (isset($_POST["submit"])) {
-    if (empty($_POST["First_name"])) {
+    if (empty($_POST["First_name"]) && empty($_POST["Last_name"]) &&
+        empty($_POST["user_name"]) && empty($_POST["email"]) &&
+        empty($_POST["password"])) {
+      echo "please fill form and register";
+    }elseif (empty($_POST["First_name"])) {
        echo "please enter first name";
     }elseif (empty($_POST["Last_name"])) {
         echo "please enter last name";
@@ -25,6 +29,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "please enter an email";
     }elseif (empty($_POST["password"])) {
         echo "please enter a password";
+    }else{
+      if (!empty($_POST["password"])) {
+        function passwordintr($password){
+          $total1 = $password;
+          $include_num = filter_input(INPUT_POST, "password", FILTER_SANITIZE_NUMBER_INT);
+          
+          if (strlen($total1) >= 8 ) {
+            //echo "you are login";
+            function isPartUppercase($total1, $include_num) {
+              if(preg_match("/[A-Z]/", $total1)===0) {
+                echo "your password most include at least one uppercase letter";
+              }else{
+                function ispartlowercase($total1, $include_num){
+                  if(preg_match("/[a-z]/", $total1)===0) {
+                    echo "your password most include at least one lowercase letter";
+                  }else{
+                    if (strlen($total1) != strlen($include_num)) {
+                      $number = filter_input(INPUT_POST, "NUM", FILTER_SANITIZE_NUMBER_INT);  
+                      if (strlen($number)) {
+                        function ispartpuntuation($total1){
+                          $include =  array("!", "@","#","$","%","&");
+                          if(preg_match("/[{$include}]/", $total1)===0) {
+                            echo "your password most atleast special character";
+                          }else{
+                            echo "you are login" . "<br>";
+                            $check = 0;
+                            $check2 = null;
+                            if (isset($_POST["submit"])) {
+                              $check ++;
+                            }
+                            echo $check . "<br>";
+                          }  
+                        }
+                        ispartpuntuation($total1);
+                      }else {
+                        echo "your password most include atleast one number";
+                      } 
+                    } else {
+                      echo "your password most cosist of atleast a later";
+                    }
+                  }
+                }
+                ispartlowercase($total1, $include_num);
+              }
+            }
+            isPartUppercase($total1, $include_num);
+          }elseif (isset($num) && strlen($total1) < 8 && !empty($num) && isset($_POST["submit"])) {
+            echo "your password most be atleast 8 digit that <br> 
+                  include uppercase , lowercase , digits and <br>
+                  special characters.";
+          }
+        }
+      }
+
+      
     }
   }
 
@@ -59,7 +118,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="test"><?php "<br>";?></div><br>
     <label for="password">password:</label>
     <input type="password" name="password" id="password" placeholder="enter a password"><br>
-    <div class="test"><?php "<br>";?></div><br>
+    <div class="test"><?php passwordintr($password) . "<br>";?></div><br>
 
     <button type="submit"  name="submit">register</button>
   </form>
